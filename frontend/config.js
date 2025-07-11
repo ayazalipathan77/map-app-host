2a01c4e97941d32cf2ab83afc9399c3fd8bd2a7c
ayaz.ali <ayaz.ali@psw.gov.pk>
2025-07-11 15:40:41 +0500

    changes to backend host url dynamically

--- frontend/config.js
+++ frontend/config.js
@@ -1,1 +1,11 @@
-export const API_BASE_URL = 'http://localhost:5111';
+let apiBaseUrl;
+
+const hostname = window.location.hostname;
+
+if (hostname === 'localhost' || hostname === '127.0.0.1') {
+    apiBaseUrl = 'http://localhost:5111';
+} else {
+    apiBaseUrl = 'https://map-app-host-backend.onrender.com';
+}
+
+export const API_BASE_URL = apiBaseUrl;
